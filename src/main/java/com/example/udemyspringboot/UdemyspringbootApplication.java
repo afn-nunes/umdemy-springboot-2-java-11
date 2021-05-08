493668d840f30cceba226102bd83e81fc6962b51
ANDRE FERREIRA NUNES <afn.nunes@gmail.com>
2021-05-08 19:05:20 -0300

    Project created

---
+++ src/main/java/com/example/udemyspringboot/UdemyspringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.udemyspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UdemyspringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UdemyspringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/udemyspringboot/UdemyspringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.udemyspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UdemyspringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
